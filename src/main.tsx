20dec6fdcf7e905744607a1341de342fb59c8018
hobby-overflow <radio_old_boy_cq@yahoo.co.jp>
2022-06-10 20:18:25 +0900

    first commit.

---
+++ src/main.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+import { Map } from "./Map";
+import "./index.css";
+
+createRoot(document.getElementById("root")!).render(<Map />);
